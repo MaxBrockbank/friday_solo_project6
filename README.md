b3dabbd8fa58907447777a1e8fb4c507ae134907
Max Brockbank <maxbrockbank1999@gmail.com>
2020-11-20 14:15:52 -0800

    add func to format numbers to corresponding currency type

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 
 ## Required Technologies
 <details>
-<summary></summary>
+<summary>See details</summary>
 
 * Node.js / Webpack
 * Modern web broswer
@@ -20,18 +20,19 @@
 
 ## Set-up Instructions
 <details>
-<summary></summary>
+<summary>See details</summary>
 
 1. Clone this repo to your computer
-2. Go to the [ExchangeRate-API](https://www.exchangerate-api.com/) website and sign up for an API key (the free plan is all you need).
-3. Create a `.env` file in the highest level of the project directory and create a global variable called `API_KEY` and set it equal to your api key that you just signed up for. Should look like: `API_KEY = (_your api key here_)
-4. Run npm i to install node_modules/
+2. Go to the [ExchangeRate-API](https://www.exchangerate-api.com/) website and sign up for an API key (the free plan is all you need, see the _Standard Endpoint_ documentation for details on the API call made).
+3. Create a `.env` file in the highest level of the project directory and create a global variable called `API_KEY` and set it equal to your api key that you just signed up for. Should look like: `API_KEY = (_your api key here_)`
+4. Run `npm i` while in the highest level of the project directory to install node_modules/
+5. Run `npm start` while in the highest level of the project directory which will launch the development server in your browser.
 
 </details>
 
 ## Technologies Used
 <details>
-<summary></summary>
+<summary>See details</summary>
 
 * HTML 
 * CSS / Bootstrap
@@ -43,24 +44,25 @@
 
 ## Known Bugs
 <details>
-<summary></summary>
+<summary>See details</summary>
 
 </details>
 
 ## Specs
 <details>
-<summary></summary>
+<summary>See details</summary>
 
 | Behavior  | Input | Output  |
 | :--- | :---: |  :---: |
 |1. Make ExchangeRate API request| | Promise|
 |2. Import API call response to main.js| | |
 |3. Check if the API call response is an error object|||
-|4. If not an error object, grab specific country conversion rate| country_code = "EUR" | conversion_rate=0.844|
+|4. If not an error object, grab specific country conversion rate| country code = "EUR" | conversion rate=0.844|
 |5. HTML dropdown selection with options whose values are set to a corresponding country code| Europe | "EUR"|
 |6. UI Logic to grab the value of the currently selected dropdown option to be set to the variable `code` attached to a button event listener|||
 
 </details>
+
 ## Legal
-* Copyright © 2020 Max Brockbank
-* This software is licensed under the MIT license
+* Copyright __©__ 2020 Max Brockbank
+* This software is licensed under the __MIT license__
--- src/main.js
+++ src/main.js
@@ -5,30 +5,55 @@ import './css/styles.css';
 import ExchangeService from './services/exchangeService.js';
 
 $(document).ready(function(){
-
+  let countryArray;
+  let ratesArray;
+  const currencies = ExchangeService.calculateExchange()
+    .then((callResponse) => {
+      if(callResponse instanceof Error){
+        throw Error (`ExchangeRate API error: ${exchangeResponse.message}`)
+      }
+      countryArray = Object.keys(callResponse.conversion_rates);
+      ratesArray = Object.values(callResponse.conversion_rates);
+      let optionsArray = [];
+      for(let i = 0; i < countryArray.length; i++){
+        optionsArray.push(`<option>${countryArray[i]}</option>`)
+      }
+      optionsArray.forEach(option => $("#country").append(option));
+    })
+    .catch((error)=>{
+      alert(`An Error Occured: ${error}`);
+    })
   $("#convert").on('submit', (event) => {
     event.preventDefault();
     let code = $("#country option:selected");
     const USD = $("#USD").val();
     const result = $("#result");
-    ExchangeService.calculateExchange()
-      .then((exchangeResponse) => {
+      currencies.then((exchangeResponse) => {
         if(exchangeResponse instanceof Error){
           throw Error (`ExchangeRate API error: ${exchangeResponse.message}`)
         }
-        const countryArray = Object.keys(exchangeResponse.conversion_rates);
-        const ratesArray = Object.values(exchangeResponse.conversion_rates);
         let output;
         for(let i = 0; i < countryArray.length; i++){
           console.log(countryArray[i]);
           if(code.val() === countryArray[i]){
-            output = `<h2>${USD} USD is equal to ${((ratesArray[i]) * USD)} ${countryArray[i]}</h2>`;
+            const formatter1 = new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD',
+              minimumFractionDigits:2
+            })
+            const formatter2 = new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: `${countryArray[i]}`,
+              minimumFractionDigits:2
+            })
+            let toConvert = formatter1.format(USD);
+            let converted = formatter2.format(((ratesArray[i]) * USD))
+            output = `<h2>${toConvert} is equal to ${converted}</h2>`;
             break;
-          } else if(code.val() === "none"){
+          } else if(code.text() === "none"){
             output = `Nothing selected, please select a valid coversion.`;
           } else {
-            output = `${code.val()} conversion is not available. Sorry about that. Please select another currency conversion.`;
-            console.log("Fail")
+            output = `${code.text()} conversion is not available. Sorry about that. Please select another currency conversion.`;
           }
         }
         result.html(output);
@@ -40,7 +65,7 @@ $(document).ready(function(){
         // console.log(USD);
       })
       .catch((error)=>{
-        console.log(error);
+        alert(`An Error Occured: ${error}`);
       })
   })
     
