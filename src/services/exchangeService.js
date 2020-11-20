d5d8c8a407c704f1df7cd2650e64c9f08b24e7c1
Max Brockbank <maxbrockbank1999@gmail.com>
2020-11-20 09:05:05 -0800

    add API call in the ExhangeService class

---
+++ src/services/exchangeService.js
@@ -0,0 +1,16 @@
+export default class ExchangeService{
+
+  static async calculateExchange(country_code){
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(response => {
+        if(!response.ok){
+          throw Error(response.message)
+        }
+        return response.json();
+      })
+      .catch(error => {
+        return error.message;
+      })
+  }
+
+}
