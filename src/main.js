fc0ea410f625788dbf53e9f89802ff8902b049c2
Max Brockbank <maxbrockbank1999@gmail.com>
2020-11-20 16:55:14 -0800

    add use of session storage

--- src/main.js
+++ src/main.js
@@ -5,71 +5,89 @@ import './css/styles.css';
 import ExchangeService from './services/exchangeService.js';
 
 $(document).ready(function(){
-  let countryArray;
-  let ratesArray;
+	let countryArray;
+	let ratesArray;
+	let currencies; 
 
-  const currencies = ExchangeService.calculateExchange()
-    .then((callResponse) => {
-      if(callResponse instanceof Error){
-        throw Error (`ExchangeRate API error: ${exchangeResponse.message}`)
-      }
-      console.log(callResponse);
-      countryArray = Object.keys(callResponse.conversion_rates);
-      ratesArray = Object.values(callResponse.conversion_rates);
-      let optionsArray = [];
-      for(let i = 0; i < countryArray.length; i++){
-        optionsArray.push(`<option>${countryArray[i]}</option>`)
-      }
-      optionsArray.forEach(option => $("#country").append(option));
-    })
-    .catch((error)=>{
-      alert(`An Error Occured: ${error}`);
-    })
+	function processCallData(data){
+		countryArray = Object.keys(data.conversion_rates);
+		ratesArray = Object.values(data.conversion_rates);
+		let optionsArray = [];
+		for(let i = 0; i < countryArray.length; i++){
+			optionsArray.push(`<option>${countryArray[i]}</option>`);
+		}
+		optionsArray.forEach(option => $("#country").append(option));
+  
+	}
 
-  $("#convert").on('submit', (event) => {
-    event.preventDefault();
-    let code = $("#country option:selected");
-    const USD = $("#USD").val();
-    const result = $("#result");
-      currencies.then((exchangeResponse) => {
-        if(exchangeResponse instanceof Error){
-          throw Error (`ExchangeRate API error: ${exchangeResponse.message}`)
-        }
-        let output;
-        for(let i = 0; i < countryArray.length; i++){
-          console.log(countryArray[i]);
-          if(code.val() === countryArray[i]){
-            const usdFormatter = new Intl.NumberFormat('en-US', {
-              style: 'currency',
-              currency: 'USD',
-              minimumFractionDigits:2
-            })
-            const otherFormatter = new Intl.NumberFormat('en-US', {
-              style: 'currency',
-              currency: `${countryArray[i]}`,
-              minimumFractionDigits:2
-            })
-            let toConvert = usdFormatter.format(USD);
-            let converted = otherFormatter.format(((ratesArray[i]) * USD))
-            output = `<h2>${toConvert} is equal to ${converted}</h2>`;
-            break;
-          } else if(code.text() === "none"){
-            output = `Nothing selected, please select a valid coversion.`;
-          } else {
-            output = `${code.text()} conversion is not available. Sorry about that. Please select another currency conversion.`;
-          }
-        }
-        result.html(output);
-        $("#country").val("none");
-        $("#USD").val("");
-        // console.log(code.val());
-        // console.log(countryArray);
-        // console.log(ratesArray);
-        // console.log(USD);
-      })
-      .catch((error)=>{
-        alert(`An Error Occured: ${error}`);
-      })
-  })
+	function formatAndPost(code, USD, results){
+		let output;
+		for(let i = 0; i < countryArray.length; i++){
+			if(code.val() === countryArray[i]){
+				const usdFormatter = new Intl.NumberFormat('en-US', {
+					style: 'currency',
+					currency: 'USD',
+					minimumFractionDigits:2
+				});
+				const otherFormatter = new Intl.NumberFormat('en-US', {
+					style: 'currency',
+					currency: `${countryArray[i]}`,
+					minimumFractionDigits:2
+				});
+				let toConvert = usdFormatter.format(USD);
+				let converted = otherFormatter.format(((ratesArray[i]) * USD));
+				output = `<h2>${toConvert} is equal to ${converted}</h2>`;
+				break;
+			} else if(code.text() === "none"){
+				output = `Nothing selected, please select a valid coversion.`;
+			} else {
+				output = `${code.text()} conversion is not available. Sorry about that. Please select another currency conversion.`;
+			}
+		}
+		results.html(output);
+		$("#country").val("none");
+		$("#USD").val("");
+	}
+
+	(function(){
+		if(sessionStorage.getItem('API_data')){
+			const storedData = JSON.parse(sessionStorage.getItem('API_data'));
+			processCallData(storedData);
+		} else {
+			currencies = ExchangeService.calculateExchange()
+				.then((callResponse) => {
+					if(callResponse instanceof Error){
+						throw Error (`ExchangeRate API error: ${callResponse.message}`);
+					}
+					sessionStorage.setItem('API_data', JSON.stringify(callResponse));
+					processCallData(callResponse);
+				})
+				.catch((error)=>{
+					alert(`An Error Occured: ${error}`);
+				});
+		}
+	})();
+  
+
+	$("#convert").on('submit', (event) => {
+		event.preventDefault();
+		let code = $("#country option:selected");
+		const USD = $("#USD").val();
+		const results = $("#result");
+
+		if(sessionStorage.getItem('API_data')){
+			formatAndPost(code, USD, results);
+		} else {
+			currencies.then((exchangeResponse) => {
+				if(exchangeResponse instanceof Error){
+					throw Error (`ExchangeRate API error: ${exchangeResponse.message}`);
+				}
+				formatAndPost(code, USD, results);
+			})
+				.catch((error)=>{
+					alert(`An Error Occured: ${error}`);
+				});
+		}
+	});
     
-})
+});
--- src/services/exchangeService.js
+++ src/services/exchangeService.js
@@ -1,16 +1,16 @@
 export default class ExchangeService{
 
-  static async calculateExchange(){
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-      .then(response => {
-        if(!response.ok){
-          throw Error(response.message)
-        }
-        return response.json();
-      })
-      .catch(error => {
-        return error.message;
-      })
-  }
+	static async calculateExchange(){
+		return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+			.then(response => {
+				if(!response.ok){
+					throw Error(response.message);
+				}
+				return response.json();
+			})
+			.catch(error => {
+				return error.message;
+			});
+	}
 
 }
